8217db77ccec2e1dd117e1ed9d42c22b538bc774
Andrea Spreafico <andreaspreafico7@gmail.com>
2022-09-03 11:30:11 +0200

    Refactor + added go

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,14 @@ WORKDIR /opt
 RUN apt update && apt install -y git wget python pip
 RUN pip install -r python_requirements.txt
 
-RUN aws/install
+RUN tools/aws/install
+
+RUN rm -rf /usr/local/go && \
+    tar -C /usr/local -xzf tools/go1.19.linux-amd64.tar.gz && \
+    export PATH=$PATH:/usr/local/go/bin
 
 RUN ./install_terraform.sh
+
+WORKDIR /
 
 CMD [ "sleep", "infinity" ]
--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-if [ ! -d aws ]; then
+if [ ! -d tools ]; then
+    mkdir tools
+fi
+
+if [ ! -d tools/aws ]; then
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
 else
     echo "aws installation dir already present"
+fi
+
+if [ ! -f tools/go1.19.linux-amd64.tar.gz ]; then
+    curl -L "https://go.dev/dl/go1.19.linux-amd64.tar.gz" -o "tools/go1.19.linux-amd64.tar.gz"
+else
+    echo "go installer already present"
 fi
 
 docker build -t mydevenv .
